9dbabfd5bb5ec96f4345b624d4c17b972026a2b3
Saad Elbeleidy <sbeleidy90@gmail.com>
2015-09-05 00:55:52 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# GraphIt
+
+Purpose: Allow anyone working on a project with SCSS to easily integrate a bar graph into their project. 
+
+## How does this work?
+
+```barGraphIt("data.csv");```
+
+This will generate a simple bar graph with the default values at the body tag. The following are optional values that can be passed to the barGraphIt() function in a JSON variable as the second parameter. Ex: `barGraphIt("data.csv", {"where": "#myChart"});`
+
+|Key|Type|Default| What it represents |
+|---|----|-------|-------------------|
+|w|Integer|500|Width in px |
+|h|Integer|500|Height in px |
+|where|String|"body"|HTML tag .class or #id where the graph should appear |
+|dataValuesLessThanOne|Boolean|True|Whether the y axis should display as % or the numbers provided |
+|margins|Array|[20,20,20,40]|Top, right, bottom, left margins |
+|yLabel|String|"Values"|The label for the y axis |
+|tickCount|Integer|10|The number of ticks on the y axis and grid if specified |
+|createGrid|Boolean|True|Whether there shall be a horizontal grid on top of the graph |
+|gridColor|String|"grey"|The color of the grid lines if they are to appear |
+
+You can check out the [index.html](index.html) file for a running example.
+
+## How do I set this up on my project?
+
+You can use this by copying and importing the barGraph.scss and barGraph.js files into your relevant directories. You can then run the barGraphIt() function in a script tag on the page that will display the graph.
+
+
+For ex:
+ * SCSS
+ 	* Copy barGraph.scss into your scss directory
+ 	* Import barGraph into your main scss file
+ 	* Compile your scss 
+ * JS
+ 	* Copy barGraph.js into your js directory
+ 	* Add the script tag in index.html linking to barGraph.js and d3.js: `<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js" charset="utf-8"></script>`
+ 	* Add the `barGraphIt(dataPath)` function in a script tag in index.html
+
+See [index.html](index.html) file for an example.
+
+
---
+++ js/barGraphIt.js
@@ -0,0 +1,130 @@
+// Credits
+// Bar Graph: mbostock from http://bl.ocks.org/mbostock/3885304
+// Grids: arete from http://stackoverflow.com/questions/15580300/proper-way-to-draw-gridlines
+
+/**
+ * Generates a bargraph with the desired color styling from beleidyBarGraph.scss
+ * @param  {string} dataPath  - the path to the data.csv file
+ * @param  {JSON} vals - optional values for creating the bar graph
+ * [	{int}		500				w = width in px
+ * 		{int} 		500				h = height in px
+ * 		{string}	"body"			where = tag .class or #id where the graph should appear (useful in case of overriding CSS for multiple graphs)
+ * 		{boolean}	true			dataValuesLessThanOne = whether the y axis should display as % or the numbers provided
+ * 		{array}		[20,20,20,40]	margins = top, right, bottom, left margins
+ * 		{string}	"Values"		yLabel = the label for the y axis
+ * 		{int}		10				tickCount = the number of ticks on the y axis and grid if specified (x axis is defined by the data)
+ * 		{boolean}	true			createGrid = whether there shall be a horizontal grid on top of the graph
+ * 		{string}	"grey"			gridColor = the color of the grid lines if they are to appear (Unfortunately I haven't been able to access this using CSS)
+ * ]
+ * @return {NaN}
+ */
+function barGraphIt(dataPath, vals){
+	// Default values 
+	console.log(vals);
+	var vals = typeof vals !== 'undefined' ? vals : {},
+		w = typeof vals["w"] !== 'undefined' ? vals["w"] : 500,
+		h = typeof vals["h"] !== 'undefined' ? vals["h"] : 500,
+		where = typeof vals["where"] !== 'undefined' ? vals["where"] : "body",
+		dataValuesLessThanOne = typeof vals["dataValuesLessThanOne"] !== 'undefined' ? vals["dataValuesLessThanOne"] : true,
+		margins = typeof vals["margins"] !== 'undefined' ? vals["margins"] : [20,20,20,40],
+		yLabel = typeof vals["yLabel"] !== 'undefined' ? vals["yLabel"] : "Values",
+		tickCount = typeof vals["tickCount"] !== 'undefined' ? vals["tickCount"] : 10,
+		createGrid = typeof vals["createGrid"] !== 'undefined' ? vals["createGrid"] : true,
+		gridColor = typeof vals["gridColor"] !== 'undefined' ? vals["gridColor"] : "grey";
+
+	console.log(where);
+	if (dataValuesLessThanOne){
+		var yAxis = "%";
+	} else{
+		var yAxis = "1";
+	}
+
+	// Define margins
+	var margin = {top: margins[0], right: margins[1], bottom: margins[2], left: margins[3]},
+    width = w - margin.left - margin.right,
+    height = h - margin.top - margin.bottom;
+
+    // Define x bounds
+	var x = d3.scale.ordinal()
+	    .rangeRoundBands([0, width], .1);
+
+	// Define y bounds
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+
+	// Define x axis and orientation
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	// Define y axis and orientation
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+	    .ticks(tickCount, "%");
+
+	// Select svg 
+	var svg = d3.select(where).append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	// Get the data and start using it
+	d3.csv(dataPath, type, function(error, data) {
+		if (error) throw error;
+
+		x.domain(data.map(function(d) { return d.letter; }));
+		y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+		svg.append("g")
+	    	.attr("class", "x axis")
+	    	.attr("transform", "translate(0," + height + ")")
+	    	.call(xAxis);
+
+		svg.append("g")
+	    		.attr("class", "y axis")
+	    		.call(yAxis)
+	    	.append("text")
+	    		.attr("transform", "rotate(-90)")
+	    		.attr("y", 5)
+	    		.attr("dy", ".71em")
+	    		.style("text-anchor", "end")
+	    		.text(yLabel);
+
+		// Add the bar graph
+		svg.selectAll(".bar")
+	    		.data(data)
+	    	.enter().append("rect")
+	    		.attr("class", "bar")
+	    		.attr("x", function(d) { return x(d.letter); })
+	    		.attr("width", x.rangeBand())
+	    		.attr("y", function(d) { return y(d.frequency); })
+	    		.attr("height", function(d) { return height - y(d.frequency); });
+
+		// Create a horizontal grid
+		if (createGrid) {
+			svg.selectAll("line.horizontalGrid").data(y.ticks(tickCount)).enter()
+    			.append("line")
+        			.attr(
+        			{
+            			"class":"horizontalGrid",
+            			"x1" : margin.right,
+            			"x2" : width,
+            			"y1" : function(d){ return y(d);},
+            			"y2" : function(d){ return y(d);},
+            			"fill" : "none",
+            			"shape-rendering" : "crispEdges",
+            			"stroke" : gridColor,
+            			"stroke-width" : "1px"
+        			});
+		}
+	});
+}
+
+
+
+function type(d) {
+  d.frequency = +d.frequency;
+  return d;
+}
